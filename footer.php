b7f5f58aeb5dbe746fc21562863762018f08051b
orjan7 <orjan77@live.se>
2021-12-05 16:17:24 +0100

    Uppdate V2

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
     <!-- Script Files -->
-    <script src="includes/javascript.js"></script>
+    <script src="includes/script.js"></script>
     <!-- <script src="https://www.google.com/recaptcha/api.js" async defer></script> -->
     <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
--- test.php
+++ test.php
@@ -1,110 +1,164 @@
 <?php
 require 'header.php';
 require 'includes/branch.php';
+$startnumber = 1;
+$sql1 = "SELECT * FROM school INNER JOIN athletes ON school.idSchool=athletes.schoolNumber INNER JOIN branch ON athletes.startNumber=branch.startNumberA WHERE athletes.startNumber=$startnumber";
+$result1 = $mysqli -> query($sql1);
+$numcountfields1 = mysqli_num_fields($result1);
+$fieldConunt = $numcountfields1-1;
 
-if ($_GET['editstudent']=1) {
-    $startnumber = 1;
-    $fromSite = 'index.php';
-    $sql1 = "SELECT * FROM school INNER JOIN athletes ON school.idSchool=athletes.schoolNumber INNER JOIN branch ON athletes.startNumber=branch.startNumberA WHERE athletes.startNumber=$startnumber";
-    $result1 = $mysqli -> query($sql1);
-    $numcountfields1 = mysqli_num_fields($result1);
-    $fieldConunt = $numcountfields1-1;
+$sql5 = "SELECT branch FROM branchHeader";
+$result5 = $mysqli -> query($sql5);
+$rowconunt5 = mysqli_num_rows($result5);
 
-    $sql2 = "SELECT * FROM school";
-    $result2 = $mysqli -> query($sql2);
+$branchCheck = array();
+while ($row1 = mysqli_fetch_array($result5)) {
+    $branchTh[] = $tab.$tab.$tab.$tab.$tab.'<th nowrap>'.$row1['branch'].'</th>'.$nl;
+    $branchName[] = "'".$row1['branch']."'";
+    $branchCheck[] = $row1['branch'];
+}
+$New_start_index = 13;
 
-    $sql5 = "SELECT branch FROM branchHeader";
-    $result5 = $mysqli -> query($sql5);
-    $rowconunt5 = mysqli_num_rows($result5);
+$branchCheck = array_combine(range($New_start_index, 
+count($branchCheck) + ($New_start_index-1)),
+array_values($branchCheck));
+while ($row3 = mysqli_fetch_array($result1)) {
+  for ($y=13; $y <= $fieldConunt; $y++) {         
+      if ($row3[$y] == 0) {
+          echo $branchCheck[$y].'<input type="checkbox" name="checkbox[]" value="'.$branchCheck[$y].'">'.$nl;
+      }
+      else {
+          echo $branchCheck[$y].'<input type="checkbox" name="checkbox[]" id="myCheck" value="'.$branchCheck[$y].'" onclick="checkBranch("$branchCheck[$y]")" checked>'.$nl;
+      }
+  }
+}
+?>
+<?php $text = '<p id="text"></p>';
+  echo $text;
+// $sql = "SELECT * FROM Result WHERE startNumberR=$startnumber";
+?>
+<script type="text/javaScript">
+  function checkBranch(var slask) {
+  // Get the checkbox
+  var checkBox = document.getElementById("myCheck").value;
+  // Get the output text
+  document.getElementById("text").innerHTML = slask;
+}
+</script>
+<?php
+// if ($_GET['editstudent']=1) {
+//     $startnumber = 1;
+//     $fromSite = 'index.php';
+//     $sql1 = "SELECT * FROM school INNER JOIN athletes ON school.idSchool=athletes.schoolNumber INNER JOIN branch ON athletes.startNumber=branch.startNumberA WHERE athletes.startNumber=$startnumber";
+//     $result1 = $mysqli -> query($sql1);
+//     $numcountfields1 = mysqli_num_fields($result1);
+//     $fieldConunt = $numcountfields1-1;
+
+//     $sql2 = "SELECT * FROM school";
+//     $result2 = $mysqli -> query($sql2);
+
+//     $sql5 = "SELECT branch FROM branchHeader";
+//     $result5 = $mysqli -> query($sql5);
+//     $rowconunt5 = mysqli_num_rows($result5);
     
-    $branchCheck = array();
-    while ($row1 = mysqli_fetch_array($result5)) {
-        $branchTh[] = $tab.$tab.$tab.$tab.$tab.'<th nowrap>'.$row1['branch'].'</th>'.$nl;
-        $branchName[] = "'".$row1['branch']."'";
-        $branchCheck[] = $row1['branch'];
-    }
-    $New_start_index = 13;
+//     $branchCheck = array();
+//     while ($row1 = mysqli_fetch_array($result5)) {
+//         $branchTh[] = $tab.$tab.$tab.$tab.$tab.'<th nowrap>'.$row1['branch'].'</th>'.$nl;
+//         $branchName[] = "'".$row1['branch']."'";
+//         $branchCheck[] = $row1['branch'];
+//     }
+//     $New_start_index = 13;
   
-    $branchCheck = array_combine(range($New_start_index, 
-    count($branchCheck) + ($New_start_index-1)),
-    array_values($branchCheck));
+//     $branchCheck = array_combine(range($New_start_index, 
+//     count($branchCheck) + ($New_start_index-1)),
+//     array_values($branchCheck));
 
     
-    while ($row3 = mysqli_fetch_array($result1)) {
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><select name="school">'.$nl;
-        while ($row1 = mysqli_fetch_array($result2)) {
-            if ($row1["school"] == $row3[2]) {
-                $student[] = $tab.$tab.$tab.$tab.$tab.'<option selected="selected" value="'.$row1["idSchool"].'">'.$row3[2].' '.$row1["idSchool"].'</option>'.$nl;
-            }
-            $student[] = $tab.$tab.$tab.$tab.$tab.'<option value="'.$row1["idSchool"].'">'.$row1["school"].' '.$row1["idSchool"].'</option>'.$nl;
-        }
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<select></td>'.$nl;
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="stage" value="'.$row3[5].'"></td>'.$nl;
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="firstName" value="'.$row3[6].'"></td>'.$nl;
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="lastName" value="'.$row3[7].'"></td>'.$nl;
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="hidden" name="startNumber" value="'.$row3[8].'">'.$row3[8].'</td>'.$nl;
-        $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="disabilities" value="'.$row3[9].'"></td>'.$nl;
-        if ($row3[10] == " ") {
-            $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="checkbox" name="photo"></td>'.$nl;
-        }
-        else {
-            $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="checkbox" name="photo" checked></td>'.$nl;
-        }
+//     while ($row3 = mysqli_fetch_array($result1)) {
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><select name="school">'.$nl;
+//         while ($row1 = mysqli_fetch_array($result2)) {
+//             if ($row1["school"] == $row3[2]) {
+//                 $student[] = $tab.$tab.$tab.$tab.$tab.'<option selected="selected" value="'.$row1["idSchool"].'">'.$row3[2].' '.$row1["idSchool"].'</option>'.$nl;
+//             }
+//             $student[] = $tab.$tab.$tab.$tab.$tab.'<option value="'.$row1["idSchool"].'">'.$row1["school"].' '.$row1["idSchool"].'</option>'.$nl;
+//         }
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<select></td>'.$nl;
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="stage" value="'.$row3[5].'"></td>'.$nl;
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="firstName" value="'.$row3[6].'"></td>'.$nl;
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="lastName" value="'.$row3[7].'"></td>'.$nl;
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="hidden" name="startNumber" value="'.$row3[8].'">'.$row3[8].'</td>'.$nl;
+//         $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="text" name="disabilities" value="'.$row3[9].'"></td>'.$nl;
+//         if ($row3[10] == " ") {
+//             $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="checkbox" name="photo"></td>'.$nl;
+//         }
+//         else {
+//             $student[] = $tab.$tab.$tab.$tab.$tab.'<td colspan="2"><input type="checkbox" name="photo" checked></td>'.$nl;
+//         }
         
-        for ($y=13; $y <= $fieldConunt; $y++) { 
+//         for ($y=13; $y <= $fieldConunt; $y++) { 
             
-            if ($row3[$y] == 0) {
-                $branch[] = $tab.$tab.$tab.$tab.$tab.'<td><input type="checkbox" name="checkbox[]" value="'.$branchCheck[$y].'"></td>'.$nl;
-            }
-            else {
-                $branch[] = $tab.$tab.$tab.$tab.$tab.'<td><input type="checkbox" name="checkbox[]" value="'.$branchCheck[$y].'" checked></td>'.$nl;
-            }
-        }
-    }
-    echo $nl.'<form action="actiontest.php" method="post">'.$nl;
-        echo $tab.'<div class="container ml-0">'.$nl;
-            echo $tab.$tab.'<table class="table">'.$nl;
-                echo $tab.$tab.$tab.'<thead>'.$nl;
-                    echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Skola</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Nivå</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Förnamn</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Efternamn</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2" nowrap>Start nr</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Skada</th>'.$nl;
-                        echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Foto</th>'.$nl;
-                    echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
-                echo $tab.$tab.$tab.'</thead>'.$nl;
-                echo $tab.$tab.$tab.'<tbody>'.$nl;
-                    echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
-                        echo implode('', $student);
-                    echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
-                    echo $tab.$tab.$tab.$tab.'<tr class="text-center ">'.$nl;
-                        echo implode('', $branchTh);
-                    echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
-                    echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
-                        echo implode('', $branch);
-                    echo $tab.$tab.$tab.$tab.'</tr>';
-                echo $tab.$tab.$tab.'</tbody>'.$nl;
-            echo $tab.$tab.'</table>'.$nl;
-            echo $tab.$tab.'<input type="hidden" name="numcountfields" value="'.$numcountfields1.'">'.$nl;
-            if (empty($_GET['gren'])) {
-                echo $tab.$tab.'<input type="hidden" name="update" value="1">'.$nl;
-            }
-            else {
-                echo $tab.$tab.'<input type="hidden" name="update" value="'.$_GET['gren'].'">'.$nl;
-            }
-            echo $tab.$tab.'<input type="hidden" name="site" value="'.$fromSite.'">'.$nl;
-            echo $tab.$tab.'<button type="submit" onclick="uppdate()" name="Spara">Spara</button>'.$nl;
-            echo $tab.'</div>'.$nl;
-        echo '</form>'.$nl;
-}
-else {
-    echo "<script type='text/javascript'>
-            alert('Välj ett start nr från startsidan.');
-                window.location='index.php';
-        </script>";
+//             if ($row3[$y] == 0) {
+//                 $branch[] = $tab.$tab.$tab.$tab.$tab.'<td><input type="checkbox" name="checkbox[]" id="myCheck" value="'.$branchCheck[$y].'" oncklick="checkbranch()"></td>'.$nl;
+//             }
+//             else {
+//                 $branch[] = $tab.$tab.$tab.$tab.$tab.'<td><input type="checkbox" name="checkbox[]" id="myCheck" value="'.$branchCheck[$y].'" oncklick="checkbranch()" checked></td>'.$nl;
+//             }
+//         }
+//     }
+//     echo '<p id="text" style="display:none>Det funderar</p>';
+//     echo '<script>function checkbranch() {
+//         var checkbox = document.getElementById("myCheck");
+//         var text = document.getElementById("text");
+//         if (checkbox.checked == true) {
+//             text.style.display = "block";
+//         } else {
+//             text.style.display = "none";
+//         }
+//     }<script>';
+//     echo $nl.'<form action="actiontest.php" method="post">'.$nl;
+//         echo $tab.'<div class="container ml-0">'.$nl;
+//             echo $tab.$tab.'<table class="table">'.$nl;
+//                 echo $tab.$tab.$tab.'<thead>'.$nl;
+//                     echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Skola</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Nivå</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Förnamn</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Efternamn</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2" nowrap>Start nr</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Skada</th>'.$nl;
+//                         echo $tab.$tab.$tab.$tab.$tab.'<th colspan="2">Foto</th>'.$nl;
+//                     echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
+//                 echo $tab.$tab.$tab.'</thead>'.$nl;
+//                 echo $tab.$tab.$tab.'<tbody>'.$nl;
+//                     echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
+//                         echo implode('', $student);
+//                     echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
+//                     echo $tab.$tab.$tab.$tab.'<tr class="text-center ">'.$nl;
+//                         echo implode('', $branchTh);
+//                     echo $tab.$tab.$tab.$tab.'</tr>'.$nl;
+//                     echo $tab.$tab.$tab.$tab.'<tr>'.$nl;
+//                         echo implode('', $branch);
+//                     echo $tab.$tab.$tab.$tab.'</tr>';
+//                 echo $tab.$tab.$tab.'</tbody>'.$nl;
+//             echo $tab.$tab.'</table>'.$nl;
+//             echo $tab.$tab.'<input type="hidden" name="numcountfields" value="'.$numcountfields1.'">'.$nl;
+//             if (empty($_GET['gren'])) {
+//                 echo $tab.$tab.'<input type="hidden" name="update" value="1">'.$nl;
+//             }
+//             else {
+//                 echo $tab.$tab.'<input type="hidden" name="update" value="'.$_GET['gren'].'">'.$nl;
+//             }
+//             echo $tab.$tab.'<input type="hidden" name="site" value="'.$fromSite.'">'.$nl;
+//             echo $tab.$tab.'<button type="submit" onclick="uppdate()" name="Spara">Spara</button>'.$nl;
+//             echo $tab.'</div>'.$nl;
+//         echo '</form>'.$nl;
+// }
+// else {
+//     echo "<script type='text/javascript'>
+//             alert('Välj ett start nr från startsidan.');
+//                 window.location='index.php';
+//         </script>";
 
-}
+// }
 require 'footer.php';
 ?>
